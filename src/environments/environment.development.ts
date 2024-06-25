fa8a3e839d789ea66244641ef735ece0df36f08a
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-06-25 13:08:22 +0200

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://api.themoviedb.org/3/movie/',
-  apiKey: '5aad91a5c30fda73b753880160e8f5b9',
-  token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWFkOTFhNWMzMGZkYTczYjc1Mzg4MDE2MGU4ZjViOSIsIm5iZiI6MTcxOTIzMjQ3Mi41MDM2ODksInN1YiI6IjY2Nzk2NzMwM2JmMzIzN2I0MzIzNmI1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SOlQGS2zZ9NmT6w-QILc6fuzukIEiZmBTCVty_qL_5g',
-};
